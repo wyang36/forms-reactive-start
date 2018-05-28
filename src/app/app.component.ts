c583860347454ea081dfdb672e18c1cce3147600
Kira Yang <wenqin.z.yang@gmail.com>
2018-05-28 15:52:18 -0400

    more form validation
    adding async form validation and preset values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,35 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log(status);
+    });
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      gender: 'male',
+      hobbies: []
+    })
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna'
+      }
     })
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -37,5 +58,18 @@ export class AppComponent implements OnInit {
     }
     //must return null if form is valid
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    })
+    return promise;
   }
 }
